8e48717e98d0137def32968b4588f1260d30654c
ok-oldking <ok-oldking@users.noreply.github.com>
2026-06-14 15:48:55 +0000

    演算入口识别修复

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import numpy as np
 from src.interaction.EfInteraction import EfInteraction
 from src.tasks.daily.finally_file import decode
 
-version = "v0.3.48"
+version = "v0.3.49"
 
 
 # 不需要修改version, Github Action打包会自动修改
