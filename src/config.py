74e477fc29894a29271cc32da22f590c2366f57d
ok-oldking <ok-oldking@users.noreply.github.com>
2026-07-09 05:47:09 +0000

    refactor: 重构汇总报告函数，更新相关调用和测试用例

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import numpy as np
 from src.interaction.EfInteraction import EfInteraction
 from src.tasks.daily.finally_file import decode
 
-version = "v0.3.81"
+version = "v0.3.82"
 
 
 # 不需要修改version, Github Action打包会自动修改
