0672db474816c4a98412b0dab8c2e160d43070f6
ok-oldking <ok-oldking@users.noreply.github.com>
2026-08-01 18:26:37 +0000

    Merge pull request #151 from CodingAQ/feat/battle-conditional-rotation

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import numpy as np
 from src.interaction.EfInteraction import EfInteraction
 from src.tasks.daily.finally_file import decode
 
-version = "v1.0.4"
+version = "v1.0.5"
 
 
 # 不需要修改version, Github Action打包会自动修改
