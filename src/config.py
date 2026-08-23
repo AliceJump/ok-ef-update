a9a202735fa1126ab910ca212d041beb4d827fbf
ok-oldking <ok-oldking@users.noreply.github.com>
2026-08-23 23:43:35 +0000

    ci: 构建等待更新仓库同步完成，修复安装包打进旧依赖集的竞态 (AliceJump)

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from src.core.email_config import email_config_option
 from src.interaction.EfInteraction import EfInteraction
 from src.tasks.daily.finally_file import decode
 
-version = "v1.0.60-beta.1"
+version = "v1.0.60-beta.2"
 
 
 # 不需要修改version, Github Action打包会自动修改
